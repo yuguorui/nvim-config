ad62f4d8e93be74547724876f71e0a29ae393938
yuguorui <yuguorui@pku.edu.cn>
2024-02-10 17:19:17 +0800

    Set maxline limits for function context
    Signed-off-by: yuguorui <yuguorui@pku.edu.cn>

--- init.lua
+++ init.lua
@@ -228,6 +228,7 @@ require("lazy").setup({
         event = "VeryLazy",
         config = function()
             require('treesitter-context').setup{
+                max_lines = 5,
             }
         end,
     },
