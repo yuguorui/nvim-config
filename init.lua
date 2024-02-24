a49fc24394a20628462138e4e077afb9bdc31c52
yuguorui <yuguorui@pku.edu.cn>
2024-02-24 22:28:41 +0800

    fix copilot completion not working when snippet is activate
    Signed-off-by: yuguorui <yuguorui@pku.edu.cn>

--- init.lua
+++ init.lua
@@ -446,8 +446,6 @@ keyset("i", "<S-TAB>", [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]], opts)
 -- <C-g>u breaks current undo, please make your own choice
 keyset("i", "<cr>", [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]], opts)
 
--- Use <c-j> to trigger snippets
-keyset("i", "<c-j>", "<Plug>(coc-snippets-expand-jump)")
 -- Use <c-space> to trigger completion
 keyset("i", "<c-space>", "coc#refresh()", {silent = true, expr = true})
 
@@ -599,6 +597,10 @@ keyset("n", "<space>k", ":<C-u>CocPrev<cr>", opts)
 
 keyset('i', '<C-k>', 'copilot#Accept("\\<CR>")', {
     expr = true,
+    noremap = true,
+    desc = "Accept copilot suggestion",
     replace_keycodes = false
 })
 vim.g.copilot_no_tab_map = true
+vim.g.coc_snippet_next = '<TAB>'
+vim.g.coc_snippet_prev = '<S-TAB>'
