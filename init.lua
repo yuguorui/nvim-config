ba29d0ba35ae762172c286bb9ec45c9c04aa2b9e
ruogui.ygr <ruogui.ygr@alibaba-inc.com>
2024-04-10 11:59:22 +0800

    Add nvim autosession deps
    Signed-off-by: ruogui.ygr <ruogui.ygr@alibaba-inc.com>

--- init.lua
+++ init.lua
@@ -307,6 +307,25 @@ require("lazy").setup({
         'github/copilot.vim',
         lazy = false
     },
+    {
+        "pysan3/autosession.nvim", -- restore previous session
+        event = { "VeryLazy" },                   -- OPTIONAL
+        -- lazy = false,                          -- If you do not want to lazy load.
+        dependencies = { "mhinz/vim-startify" },  -- OPTIONAL: Used for `:AutoSessionGlobal`
+        config = function(_, opts)
+            require("autosession").setup({
+                msg = nil, -- string: printed when startup is completed
+                restore_on_setup = true, -- boolean: If true, automatically restore session on nvim startup
+                warn_on_setup = false, -- boolean: If true, warning shown when no `.session.vim` is found
+                autosave_on_quit = true, -- boolean: If true, automatically overwrites sessionfile if exists
+                save_session_global_dir = vim.g.startify_session_dir or vim.fn.stdpath("data") .. "/session", -- string
+                --                        dir path to where global session files should be stored.
+                --                        global sessions will show up in startify screen as dirname of the session
+                sessionfile_name = ".session.vim", -- string: default name of sessionfile. better be .gitignored
+                disable_envvar = "NVIM_DISABLE_AUTOSESSION", -- string: disable this plugin altogether when this envvar is found
+            })
+       end
+    },
 })
 
 vim.cmd([[
@@ -554,7 +573,6 @@ keyset("i", "<C-b>",
        'coc#float#has_scroll() ? "<c-r>=coc#float#scroll(0)<cr>" : "<Left>"', opts)
 keyset("v", "<C-f>", 'coc#float#has_scroll() ? coc#float#scroll(1) : "<C-f>"', opts)
 keyset("v", "<C-b>", 'coc#float#has_scroll() ? coc#float#scroll(0) : "<C-b>"', opts)
-
 
 -- Use CTRL-S for selections ranges
 -- Requires 'textDocument/selectionRange' support of language server
