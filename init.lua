302c3954bea8bd5439abc0481312978e389bb912
yuguorui <yuguorui@pku.edu.cn>
2024-04-12 17:28:06 +0800

    Add whitespace.nvim for whitespace highlight
    Signed-off-by: yuguorui <yuguorui@pku.edu.cn>

--- init.lua
+++ init.lua
@@ -349,6 +349,28 @@ require("lazy").setup({
             })
         end
     },
+    {
+        'johnfrankmorgan/whitespace.nvim',
+        event = { "BufReadPost", "BufNewFile" },
+        opts = {
+            -- configuration options and their defaults
+
+            -- `highlight` configures which highlight is used to display
+            -- trailing whitespace
+            highlight = 'DiffDelete',
+
+            -- `ignored_filetypes` configures which filetypes to ignore when
+            -- displaying trailing whitespace
+            ignored_filetypes = { 'TelescopePrompt', 'Trouble', 'help' },
+
+            -- `ignore_terminal` configures whether to ignore terminal buffers
+            ignore_terminal = true,
+
+            -- `return_cursor` configures if cursor should return to previous
+            -- position after trimming whitespace
+            return_cursor = true,
+        },
+    },
 })
 
 vim.cmd([[
